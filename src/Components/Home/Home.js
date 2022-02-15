98b2759635514834a3593dd2d7b637f4696efef3
Noah Taylor <noahnt@live.ca>
2022-02-14 22:53:50 -0330

    Updates to home component
    - Updated design of home section
    - Added pulse image

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,13 +4,22 @@ import aeolusLogo from "./Images/aeolus.png";
 
 function Home() {
   return (
-    <div id="home" className="home-section">
-      <div>
-        <img className="aeolusLogo" src={aeolusLogo} alt="" />
+    <div className="home-container">
+      <div id="home" className="home-section">
+        <div>
+          <img className="aeolusLogo" src={aeolusLogo} alt="" />
+        </div>
+        <div>
+          <h2 className="home-caption">Breathe Easy.</h2>
+          <p className="home-descript">
+            Aeolus provides robust and one-of a kind solutions to global
+            problems. Innovation, at a fair and global price. The health
+            equality gap became even more apparent during hte pandemic and
+            Aeolus is here to adress it.
+          </p>
+        </div>
       </div>
-      <div>
-        <h2 className="home-caption">Breathe Easy.</h2>
-      </div>
+      <div className="heartbeat">Heart</div>
     </div>
   );
 }
