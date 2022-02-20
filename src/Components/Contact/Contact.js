5e2a90bb8d1cfcc97fef64b505164931fdadd461
Noah Taylor <noahnt@live.ca>
2022-02-20 12:47:00 -0330

    Updates to styling of contact section
    - Updated styling of contact form and started adding responsiveness

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,28 +3,53 @@ import "./Contact.css";
 
 function Contact() {
   return (
-    <div id="contact" className="contact-section">
-      <h2 className="contact-heading">Want to learn more? Get in touch.</h2>
-      <form action="">
-        <label htmlFor="name" className="contact-text">
-          Your Name
-        </label>
-        <br />
-        <input id="name" type="text" className="contact-input" />
-        <br />
-        <label htmlFor="email" className="contact-text">
-          Your Email
-        </label>
-        <br />
-        <input id="name" type="text" className="contact-input" />
-        <br />
-        <label htmlFor="message" className="contact-text">
-          What would you like to say?
-        </label>
-        <br />
-        <textarea id="name" className="contact-input" />
-        <br />
-        <button className="contact-button">Submit</button>
+    <div id="contact" className="contact">
+      <div className="contact-outside-heading">
+        <h2 className="contact-heading">Want to learn more?</h2>
+        <h3 className="contact-heading">Get in touch!</h3>
+      </div>
+      <form className="contact-section" action="">
+        <h2 className="contact-inside-heading">
+          Want to learn more? Get in touch!
+        </h2>
+        <div className="contact-info">
+          <div className="contact-row">
+            <label htmlFor="name" className="contact-text">
+              Your Name:
+            </label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Your Name"
+              className="contact-input"
+            />
+          </div>
+          <div className="contact-row">
+            <label htmlFor="email" className="contact-text">
+              Your Email:
+            </label>
+            <input
+              id="name"
+              type="email"
+              placeholder="Your Email"
+              className="contact-input"
+            />
+          </div>
+        </div>
+        <div className="contact-message-row">
+          <label htmlFor="message" className="contact-text">
+            What would you like to say?
+          </label>
+          <textarea
+            id="name"
+            type="text"
+            placeholder="Your Message..."
+            className="contact-text-area"
+          />
+        </div>
+        <div className="button-row">
+          <button className="contact-button">Submit</button>
+        </div>
       </form>
     </div>
   );
