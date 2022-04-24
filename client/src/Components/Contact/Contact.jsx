70f446362091ad2b63275e657f3ecd7dd04755d2
Noah Taylor <noahnt@live.ca>
2022-04-24 10:08:23 -0230

    Updated Path
    - removed hard-coded localhost path

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -65,7 +65,7 @@ function ContactForm(props) {
         message: message,
       };
 
-      let response = await fetch("http://localhost:5000/contact", {
+      let response = await fetch("/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
