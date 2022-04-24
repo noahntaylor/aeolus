d5ec46e824c13529c47a85c30da3aa3c7bb529df
Noah Taylor <noahnt@live.ca>
2022-04-24 09:44:01 -0230

    Restructured folders
    - Moved react code into client folder
    - Removed firebase code

--- client/src/Components/Product/Product.jsx
+++ client/src/Components/Product/Product.jsx
@@ -9,6 +9,9 @@ function Product() {
   const secondProductTitle = "More About Product...";
   const secondProductText =
     "Aeolus has developed a robust pneumatic and portable ventilator and cost effective ventilator. Katie Stone, 22, won the Genesis Centre's Pitch and Pick competition with the Aeolus Portable Ventilation System, a prototype that will hopefully provide a cost-effective alternative to traditional COVID-19 ventilators. She said the project started as a capstone design project for her degree, along with work from partners Desiree Van Heerden and Rachel Tobin, but Stone decided   to keep working on the project herself after graduation.";
+  const thirdProductTitle = "Last About Product...";
+  const thirdProductText =
+    "Aeolus has developed a robust pneumatic and portable ventilator and cost effective ventilator. Katie Stone, 22, won the Genesis Centre's Pitch and Pick competition with the Aeolus Portable Ventilation System, a prototype that will hopefully provide a cost-effective alternative to traditional COVID-19 ventilators. She said the project started as a capstone design project for her degree, along with work from partners Desiree Van Heerden and Rachel Tobin, but Stone decided   to keep working on the project herself after graduation.";
 
   return (
     <div id="product" className="product">
@@ -26,6 +29,13 @@ function Product() {
         <h2 className="product-heading">{secondProductTitle}</h2>
         <p className="product-text">{secondProductText}</p>
       </div>
+      {/* <div className="product-section">
+        <h2 className="product-heading">{thirdProductTitle}</h2>
+        <p className="product-text">{thirdProductText}</p>
+      </div>
+      <div className="product-img-section">
+        <img className="img" src={ventilator} alt="" />
+      </div> */}
     </div>
   );
 }
