c9d005c6f0baad41aee89a9c32ec92b7dd53a02e
Noah Taylor <noahntaylor@gmail.com>
2022-04-25 22:32:25 -0230

    Updated Product Section Layout
    - Improved layout of product section with 3 components

--- client/src/Components/Product/Product.jsx
+++ client/src/Components/Product/Product.jsx
@@ -13,19 +13,19 @@ function Product() {
       <div className="product-img-section">
         <img className="img" src={ProductData.productImage} alt="" />
       </div>
-      <div className="product-img-section-right">
-        <img className="img" src={ProductData.secondProductImage} alt="" />
-      </div>
-      <div className="product-section-right">
+      <div className="product-section second">
         <h2 className="product-heading">{ProductData.secondProductTitle}</h2>
         <p className="product-text">{ProductData.secondProductText}</p>
       </div>
-      <div className="product-section">
+      <div className="product-img-section">
+        <img className="img" src={ProductData.secondProductImage} alt="" />
+      </div>
+      <div className="product-img-section-right">
+        <img className="img" src={ProductData.thirdProductImage} alt="" />
+      </div>
+      <div className="product-section-right">
         <h2 className="product-heading">{ProductData.thirdProductTitle}</h2>
         <p className="product-text">{ProductData.thirdProductText}</p>
-      </div>
-      <div className="product-img-section last">
-        <img className="img" src={ProductData.thirdProductImage} alt="" />
       </div>
     </div>
   );
